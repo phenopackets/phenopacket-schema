6e95bd614fd974217abad2a1e62b3c76033a3968
iimpulse <michael.gargano@jax.org>
2021-09-13 19:38:29 -0400

    fixing deploy for v2, produces temp dir with python module

--- deploy-python.sh
+++ deploy-python.sh
@@ -5,29 +5,33 @@ echo "$TEMP_DIRECTORY"
 TEMP_DIRECTORY_PYTHON_MODULE="$TEMP_DIRECTORY/phenopackets"
 TEMP_DIRECTORY_TESTS_MODULE="$TEMP_DIRECTORY/tests"
 TEMP_DIRECTORY_VIRTUAL_ENV="$TEMP_DIRECTORY/phenopackets-venv"
-
+declare -a pyfiles=("base" "phenopackets" "biosample" "disease" "genome" "individual" "interpretation" "medical_action" "measurement" "meta_data" "pedigree" "phenotypic_feature" "vrsatile")
 # Functions
 createInitFile(){
   echo "import pkg_resources" >> "$TEMP_DIRECTORY/phenopackets/__init__.py"
   echo "__version__ = pkg_resources.get_distribution('phenopackets').version"   >> "$TEMP_DIRECTORY/phenopackets/__init__.py"
-  echo "from .base_pb2 import *"   >> "$TEMP_DIRECTORY/phenopackets/__init__.py"
-  echo "from .phenopackets_pb2 import *"   >> "$TEMP_DIRECTORY/phenopackets/__init__.py"
-  echo "from .interpretation_pb2 import *"   >> "$TEMP_DIRECTORY/phenopackets/__init__.py"
+  for i in "${pyfiles[@]}"
+  do
+     echo "from .${i}_pb2 import *"  >> "$TEMP_DIRECTORY/phenopackets/__init__.py"
+  done
 }
 
 replaceImports(){
-  # Interpretation Replace
-  sed -i 's/import base_pb2/from . import base_pb2/g' "$TEMP_DIRECTORY_PYTHON_MODULE/interpretation_pb2.py"
-  sed -i 's/import phenopackets_pb2/from . import phenopackets_pb2/g' "$TEMP_DIRECTORY_PYTHON_MODULE/interpretation_pb2.py"
-
-  # Phenopackets Replace
-  sed -i 's/import base_pb2/from . import base_pb2/g' "$TEMP_DIRECTORY_PYTHON_MODULE/phenopackets_pb2.py"
+    for i in "${pyfiles[@]}"
+  do
+      sed -i "" 's/from phenopackets.schema.v2.core/from . /g' "$TEMP_DIRECTORY_PYTHON_MODULE/${i}_pb2.py"
+      sed -i "" 's/from ga4gh.vrsatile.v1/from . /g' "$TEMP_DIRECTORY_PYTHON_MODULE/${i}_pb2.py"
+      sed -i "" 's/from ga4gh.vrs.v1/from . /g' "$TEMP_DIRECTORY_PYTHON_MODULE/${i}_pb2.py"
+  done
 }
 
 # Create python module
 mkdir $TEMP_DIRECTORY_PYTHON_MODULE
 createInitFile
-cp ./target/generated-sources/protobuf/python/* $TEMP_DIRECTORY_PYTHON_MODULE
+cp ./target/generated-sources/protobuf/python/phenopackets/schema/v2/phenopackets_pb2.py $TEMP_DIRECTORY_PYTHON_MODULE
+cp ./target/generated-sources/protobuf/python/phenopackets/schema/v2/core/* $TEMP_DIRECTORY_PYTHON_MODULE
+cp ./target/generated-sources/protobuf/python/ga4gh/vrsatile/v1/vrsatile_pb2.py $TEMP_DIRECTORY_PYTHON_MODULE
+cp ./target/generated-sources/protobuf/python/ga4gh/vrs/v1/vrs_pb2.py $TEMP_DIRECTORY_PYTHON_MODULE
 replaceImports
 # Create tests module
 mkdir $TEMP_DIRECTORY_TESTS_MODULE
--- src/test/python/phenopackets_test.py
+++ src/test/python/phenopackets_test.py
@@ -1,7 +1,3 @@
-# n.b. the base_pb2 and phenopackets_pb2 files are located in the target/generated-sources/protobuf/python
-# directory once the project has built. This file *should* run when done so via IntelliJ/PyCharm but otherwise
-# some file hackery will be required.
-
 import os
 import unittest
 
@@ -12,17 +8,16 @@ from phenopackets import Individual, Sex, PhenotypicFeature, OntologyClass, Phen
 
 
 class PhenopacketsTest(unittest.TestCase):
+    path = os.path.dirname(os.path.abspath(__file__))
     subject = Individual(id="Zaphod", sex="MALE", date_of_birth=Timestamp(seconds=-123456798))
     phenotypic_features = [PhenotypicFeature(type=OntologyClass(id="HG2G:00001", label="Hoopy")),
                            PhenotypicFeature(type=OntologyClass(id="HG2G:00002", label="Frood"))
                            ]
     phenopacket = Phenopacket(id="PPKT:1", subject=subject, phenotypic_features=phenotypic_features)
     test_json_file = "test.json"
-
     def test_phenopacket_round_trip(self):
         with open(self.test_json_file, 'w') as jsfile:
-            json = MessageToJson(self.phenopacket)
-            jsfile.write(json)
+            jsfile.write(MessageToJson(self.phenopacket))
         with open(self.test_json_file, 'r') as jsfile:
             phenopacket = Parse(message=Phenopacket(), text=jsfile.read())
             self.assertEqual(phenopacket.subject.id, "Zaphod")
@@ -32,6 +27,30 @@ class PhenopacketsTest(unittest.TestCase):
                 expected_term = expected_feature.type
                 self.assertEqual(term.id, expected_term.id)
                 self.assertEqual(term.label, expected_term.label)
+
+    def test_phenopacket_covid(self):
+        with(open(os.path.join(self.path,"covid19.json"), 'r')) as covid19:
+            phenopacket = Parse(message=Phenopacket(), text=covid19.read())
+            self.assertEqual("P123542", phenopacket.subject.id)
+            self.assertEqual(11, len(phenopacket.phenotypic_features))
+            self.assertEqual(2, len(phenopacket.measurements))
+            self.assertEqual("NCIT:C113237", phenopacket.measurements[0].assay.id)
+            self.assertEqual("LOINC:26474-7", phenopacket.measurements[1].assay.id)
+            self.assertEqual("MONDO:0005015", phenopacket.diseases[0].term.id)
+            self.assertEqual("cardiomyopathy", phenopacket.diseases[1].term.label)
+            self.assertNotEqual(None, phenopacket.diseases[2].onset.timestamp)
+
+    def test_phenopacket_mcahs1(self):
+        with(open(os.path.join(self.path,"mcahs1.json"), 'r')) as covid19:
+            phenopacket = Parse(message=Phenopacket(), text=covid19.read())
+            self.assertEqual("proband A", phenopacket.subject.id)
+            self.assertEqual(1, phenopacket.subject.sex)
+            self.assertEqual(3, phenopacket.interpretations[0].progress_status)
+            self.assertEqual("Bethlem myopathy 1", phenopacket.interpretations[0].diagnosis.disease.label)
+            self.assertEqual(5, phenopacket.interpretations[0].diagnosis.genomic_interpretations[0].variant_interpretation.acmg_pathogenicity_classification)
+            self.assertEqual("GENO:0000135", phenopacket.interpretations[0].diagnosis.genomic_interpretations[0].variant_interpretation.variation_descriptor.allelic_state.id)
+
+
 
     @classmethod
     def tearDownClass(cls) -> None:
