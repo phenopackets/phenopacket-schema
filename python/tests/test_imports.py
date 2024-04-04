c9871081c7e951ae6c91486252d1ce8eb5bb33be
iimpulse <Michael.Gargano@jax.org>
2024-04-04 15:46:35 -0600

    reworking for backwards compat

--- python/tests/test_imports.py
+++ python/tests/test_imports.py
@@ -4,6 +4,8 @@ class TestPhenopacket:
 
     def test_create(self):
         from phenopackets.schema.v2.phenopackets_pb2 import Phenopacket
+        from phenopackets import Variation
+        from ga4gh.vrs.v1.vrs_pb2 import Variation
         pp = Phenopacket()
 
         assert isinstance(pp, Phenopacket)
