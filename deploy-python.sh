5ed24419dafdf71f91cf8d06938228086007666e
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-04-05 10:26:41 -0400

    Revert changes to `pom.xml`, do everything in the deployment script.

--- deploy-python.sh
+++ deploy-python.sh
@@ -1,7 +1,20 @@
+#!/usr/bin/env bash
+
 # Build and Deploy Python Package
+# We assume the script is ran from the top-level repository folder as ./deploy-python.sh
+
 DIRECTORY=./python
 echo "Building phenopacket distribution files in directory at $DIRECTORY"
-TEMP_DIRECTORY_VIRTUAL_ENV="phenopackets-venv"
+
+# Ensure we generated the protobuf Python files.
+./mvnw clean package
+
+# Copy the generated files into the target folder
+cp -r target/generated-sources/protobuf/python/* ${DIRECTORY}/src/
+# Add module file to re-export the v2.0.2 classes to maintain backwards compatibility
+# until we remove the classes.
+cp ${DIRECTORY}/config/__init__.py ${DIRECTORY}/src/phenopackets/
+
 cd $DIRECTORY || { echo "Deployment FAILED. Couldn't find directory" ; exit 1; }
 createVirtualEnvironment(){
   echo "Creating Python virtual environment at ${1}"
@@ -18,16 +31,17 @@ createVirtualEnvironment(){
 }
 
 # Create Python venv in virtual directory
+TEMP_DIRECTORY_VIRTUAL_ENV="phenopackets-venv"
 createVirtualEnvironment $TEMP_DIRECTORY_VIRTUAL_ENV
 # shellcheck disable=SC1090
 source "$TEMP_DIRECTORY_VIRTUAL_ENV/bin/activate"
-# Dependencies for building/deploying
-python3 -m pip install build twine || { echo "Deployment FAILED. Failed to install python dependencies" ; exit 1; }
-# Rexport module definition until v3
-cp ./config/__init__.py ./src/phenopackets/
+
 # Test
-python3 -m pip install .[test]
+python3 -m pip install ".[test]"
 pytest || { echo "Deployment FAILED. Unittest Failure" ; exit 1; }
+
+# Install dependencies for building/deploying
+python3 -m pip install build twine || { echo "Deployment FAILED. Failed to install python dependencies" ; exit 1; }
 # Build
 python3 -m build || { echo "Deployment FAILED. Building python package" ; exit 1; }
 # Deploy - Remove --repository testpypi flag for production.
@@ -39,5 +53,5 @@ else
   echo "Python Release was prepared successfully. No release argument provided, use one of [release-prod, release-test] to make the production/test release."
 fi
 
-
-
+# Clean up
+rm -rf build dist src/*
