d933390fc02111ca2b3067e85828fb9ddd2b2a9d
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-04-05 09:53:27 -0400

    Finish testing.

--- python/tests/test_imports.py
+++ python/tests/test_imports.py
@@ -1,11 +1,104 @@
+class TestImportsV202:
+    """
+    Test that the imports which we had prior `v2.0.2.post2` work.
+    """
 
+    def test_singular_import(self):
+        from phenopackets import Phenopacket
 
-class TestPhenopacket:
-
-    def test_create(self):
-        from phenopackets.schema.v2.phenopackets_pb2 import Phenopacket
-        from phenopackets import Variation
-        from ga4gh.vrs.v1.vrs_pb2 import Variation
-        pp = Phenopacket()
+        pp = Phenopacket(id='retinoblastoma.id')
 
         assert isinstance(pp, Phenopacket)
+
+    def test_import_module(self):
+        import phenopackets as pp2
+
+        pp = pp2.Phenopacket(id='retinoblastoma.id')
+
+        assert isinstance(pp, pp2.Phenopacket)
+
+    def test_import_everything(self):
+        import phenopackets
+
+        pp = phenopackets.Phenopacket(id='retinoblastoma.id')
+
+        assert isinstance(pp, phenopackets.Phenopacket)
+
+    def test_import_all_sorts_of_stuff(self):
+        import phenopackets as pps
+
+        payload = (
+            pps.OntologyClass, pps.Individual, pps.PhenotypicFeature, pps.Measurement, pps.Biosample,
+            pps.Interpretation, pps.Disease, pps.MedicalAction, pps.File, pps.MetaData, pps.Family, pps.Cohort,
+        )
+
+        for clz in payload:
+            x = clz()
+            assert isinstance(x, clz)
+
+
+class TestImportsPostV202:
+    """
+    Test that the imports which we have at and after `v2.0.2.post2` work.
+    """
+
+    def test_singular_import(self):
+        from phenopackets.schema.v2.phenopackets_pb2 import Phenopacket
+
+        pp = Phenopacket(id='retinoblastoma.id')
+
+        assert isinstance(pp, Phenopacket)
+
+    def test_import_module(self):
+        import phenopackets.schema.v2.phenopackets_pb2 as pp2
+
+        pp = pp2.Phenopacket(id='retinoblastoma.id')
+
+        assert isinstance(pp, pp2.Phenopacket)
+
+    def test_import_everything(self):
+        import phenopackets
+
+        pp = phenopackets.schema.v2.phenopackets_pb2.Phenopacket(id='retinoblastoma.id')
+
+        assert isinstance(pp, phenopackets.schema.v2.phenopackets_pb2.Phenopacket)
+
+    def test_import_v1(self):
+        from phenopackets.schema.v1.phenopackets_pb2 import Phenopacket
+
+        p = Phenopacket()
+        assert isinstance(p, Phenopacket)
+
+
+class TestImportVrsatile:
+
+    def test_singular_import(self):
+        from ga4gh.vrsatile.v1.vrsatile_pb2 import Extension
+
+        e = Extension()
+
+        assert isinstance(e, Extension)
+
+    def test_import_vrs(self):
+        import ga4gh.vrsatile.v1.vrsatile_pb2 as vrsatile
+
+        e = vrsatile.Extension()
+
+        assert isinstance(e, vrsatile.Extension)
+
+
+class TestImportVrs:
+
+    def test_singular_import(self):
+        from ga4gh.vrs.v1.vrs_pb2 import Number
+
+        number = Number()
+
+        assert isinstance(number, Number)
+
+    def test_import_vrs(self):
+        import ga4gh.vrs.v1.vrs_pb2 as vrs
+
+        n = vrs.Number()
+
+        assert isinstance(n, vrs.Number)
--- python/tests/test_phenopackets.py
+++ python/tests/test_phenopackets.py
@@ -1,62 +1,68 @@
 import os
 
+import pytest
+
 from google.protobuf.json_format import Parse, MessageToJson
 from google.protobuf.timestamp_pb2 import Timestamp
 from phenopackets import Individual, Sex, PhenotypicFeature, OntologyClass, Phenopacket
 
-# This will break when we deprecate the new stuff.
-class PhenopacketsTest:
-    path = os.path.dirname(os.path.abspath(__file__))
-    subject = Individual(id="Zaphod", sex="MALE", date_of_birth=Timestamp(seconds=-123456798))
-    phenotypic_features = [PhenotypicFeature(type=OntologyClass(id="HG2G:00001", label="Hoopy")),
-                           PhenotypicFeature(type=OntologyClass(id="HG2G:00002", label="Frood"))
-                           ]
-    phenopacket = Phenopacket(id="PPKT:1", subject=subject, phenotypic_features=phenotypic_features)
-    test_json_file = "test.json"
-    def test_phenopacket_round_trip(self):
-        with open(self.test_json_file, 'w') as jsfile:
-            jsfile.write(MessageToJson(self.phenopacket))
-        with open(self.test_json_file, 'r') as jsfile:
-            phenopacket = Parse(message=Phenopacket(), text=jsfile.read())
-            self.assertEqual(phenopacket.subject.id, "Zaphod")
-            self.assertEqual(Sex.Name(phenopacket.subject.sex), "MALE")
-            for phenotypic_feature, expected_feature in zip(phenopacket.phenotypic_features, self.phenotypic_features):
-                term = phenotypic_feature.type
-                expected_term = expected_feature.type
-                self.assertEqual(term.id, expected_term.id)
-                self.assertEqual(term.label, expected_term.label)
 
-    def test_phenopacket_covid(self):
-        with(open(os.path.join(self.path,"covid19.json"), 'r')) as covid19:
-            phenopacket = Parse(message=Phenopacket(), text=covid19.read())
-            self.assertEqual("P123542", phenopacket.subject.id)
-            self.assertEqual(11, len(phenopacket.phenotypic_features))
-            self.assertEqual(2, len(phenopacket.measurements))
-            self.assertEqual("NCIT:C113237", phenopacket.measurements[0].assay.id)
-            self.assertEqual("LOINC:26474-7", phenopacket.measurements[1].assay.id)
-            self.assertEqual("MONDO:0005015", phenopacket.diseases[0].term.id)
-            self.assertEqual("cardiomyopathy", phenopacket.diseases[1].term.label)
-            self.assertNotEqual(None, phenopacket.diseases[2].onset.timestamp)
+# This will break when we deprecate the top-level import.
+class TestPhenopackets:
 
-    def test_phenopacket_mcahs1(self):
-        with(open(os.path.join(self.path,"mcahs1.json"), 'r')) as covid19:
-            phenopacket = Parse(message=Phenopacket(), text=covid19.read())
-            self.assertEqual("proband A", phenopacket.subject.id)
-            self.assertEqual(1, phenopacket.subject.sex)
-            self.assertEqual(3, phenopacket.interpretations[0].progress_status)
-            self.assertEqual("Bethlem myopathy 1", phenopacket.interpretations[0].diagnosis.disease.label)
-            self.assertEqual(5, phenopacket.interpretations[0].diagnosis.genomic_interpretations[0].variant_interpretation.acmg_pathogenicity_classification)
-            self.assertEqual("GENO:0000135", phenopacket.interpretations[0].diagnosis.genomic_interpretations[0].variant_interpretation.variation_descriptor.allelic_state.id)
+    @pytest.fixture
+    def phenopacket(self) -> Phenopacket:
+        subject = Individual(id="Zaphod", sex="MALE", date_of_birth=Timestamp(seconds=-123456798))
+        phenotypic_features = (
+            PhenotypicFeature(type=OntologyClass(id="HG2G:00001", label="Hoopy")),
+            PhenotypicFeature(type=OntologyClass(id="HG2G:00002", label="Frood")),
+        )
 
+        return Phenopacket(id="PPKT:1", subject=subject, phenotypic_features=phenotypic_features)
 
+    def test_phenopacket_round_trip(
+        self,
+        phenopacket: Phenopacket,
+    ):
+        output: str = MessageToJson(phenopacket)
 
-    @classmethod
-    def tearDownClass(cls) -> None:
-        try:
-            os.remove(cls.test_json_file)
-        except OSError as e:
-            print("Error: %s - %s." % (e.filename, e.strerror))
+        back = Parse(message=Phenopacket(), text=output)
 
+        assert back.subject.id == "Zaphod"
+        assert Sex.Name(back.subject.sex) == "MALE"
+        for phenotypic_feature, expected_feature in zip(back.phenotypic_features, phenopacket.phenotypic_features):
+            term = phenotypic_feature.type
+            expected_term = expected_feature.type
+            assert term.id == expected_term.id
+            assert term.label == expected_term.label
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_phenopacket_covid(
+            self,
+            fpath_test_dir: str,
+    ):
+        with open(os.path.join(fpath_test_dir, "covid19.json")) as fh:
+            pp = Parse(message=Phenopacket(), text=fh.read())
+
+        assert pp.subject.id == "P123542"
+        assert len(pp.phenotypic_features) == 11
+        assert len(pp.measurements) == 2
+        assert pp.measurements[0].assay.id == "NCIT:C113237"
+        assert pp.measurements[1].assay.id == "LOINC:26474-7"
+        assert pp.diseases[0].term.id == "MONDO:0005015"
+        assert pp.diseases[1].term.label == "cardiomyopathy"
+
+        assert pp.diseases[2].onset.timestamp is not None
+
+    def test_phenopacket_mcahs1(
+            self,
+            fpath_test_dir: str,
+    ):
+        with open(os.path.join(fpath_test_dir, "mcahs1.json")) as fh:
+            pp = Parse(message=Phenopacket(), text=fh.read())
+
+        assert pp.subject.id == "proband A"
+        assert pp.subject.sex == 1
+        assert pp.interpretations[0].progress_status == 3
+        assert pp.interpretations[0].diagnosis.disease.label == "Bethlem myopathy 1"
+        assert pp.interpretations[0].diagnosis.genomic_interpretations[0].variant_interpretation.acmg_pathogenicity_classification == 5
+        assert pp.interpretations[0].diagnosis.genomic_interpretations[0].variant_interpretation.variation_descriptor.allelic_state.id == "GENO:0000135"
